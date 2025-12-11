6295a2e41074f3b55c83af2dc046c3bc6cca3125
Ranjita Thapa Chhetri <ranjita.231526@ncit.edu.np>
2025-12-11 20:47:46 +0545

    added output_variable file

---
+++ output_variables.py
@@ -0,0 +1,18 @@
+# x = "python"
+# y = "is"
+# z = "awesome"
+# print(x, y, z)
+
+# x = "awesome"
+# y = "is"
+# z = "awesome"
+# print(x + y + z)
+
+# x = 5
+# y = 10
+# print(x + y)
+
+x = 5
+y = "John"
+print(x , y)
+
