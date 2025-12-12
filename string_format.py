898e07052c1ff7d0b6ffb4c077d51204f6928a4f
Ranjita Thapa Chhetri <ranjita.231526@ncit.edu.np>
2025-12-12 19:32:30 +0545

    adding string_format.py

---
+++ string_format.py
@@ -0,0 +1,10 @@
+# age = 36
+# txt = f"My name is john, and I am {age}"
+# print(txt)
+
+
+price = 49
+# txt = f"The price is {price:.2f} dollars"
+
+txt =f"the price is {price} dollars"
+print(txt)
