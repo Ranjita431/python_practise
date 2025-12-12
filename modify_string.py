a27b3df1a47c90ba517bbf1aa1382809b7b03809
Ranjita Thapa Chhetri <ranjita.231526@ncit.edu.np>
2025-12-12 19:14:39 +0545

    adding modify_string.py file

---
+++ modify_string.py
@@ -0,0 +1,9 @@
+a = "hello everyone"
+# print(a.capitalize())  # Capitalize the first letter
+# print(a.title())  # Title Case
+# print(a.upper())  # Upper Case
+# print(a.lower())  # Lower Case
+# print(a.strip())
+# print(a.split(" , "))  # Split by comma and space
+# print(a.find("every"))  # Find substring
+
