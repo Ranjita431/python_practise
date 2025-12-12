a7db86950b941d5f9f9b147eb69c3c73d8fed7b3
Ranjita Thapa Chhetri <ranjita.231526@ncit.edu.np>
2025-12-12 18:54:37 +0545

    adding strings.py file

---
+++ strings.py
@@ -0,0 +1,20 @@
+# print("Hello")
+# print("lorem")
+
+# a = "Hello"
+# print(a)
+
+b = "hello , wolrd>"
+print(b[1:4:2])  # slicing
+
+
+print(b.lower())  # lower case
+print(b.upper())  # upper case
+print(b.replace("h", "j"))  # replace h with j
+print(b.split(","))  # split by ,
+print(b.strip(">"))  # remove > from the end
+print(b.startswith("h"))  # check if starts with h
+print(b.endswith("d>"))  # check if ends with d>
+print(len(b))  # length of the string
+print(b.index("w"))  # index of w
+
