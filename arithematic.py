f1b094cfe2c30b50a1844339f322be9d0f5f8986
Ranjita Thapa Chhetri <ranjita.231526@ncit.edu.np>
2025-12-13 23:47:25 +0545

    adding arithematic.py file

---
+++ arithematic.py
@@ -0,0 +1,13 @@
+x = 15
+y = 4
+
+print(x + y)  # Addition
+print(x - y)  # Subtraction
+
+print(x * y)  # Multiplication
+print(x / y)  # Division
+print(x // y) # Floor Division
+
+print(x % y)  # Modulus
+print(x ** y) # Exponentiation
+print((x + y) * (x - y))  # Combined Operations
