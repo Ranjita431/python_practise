50d6116cbbc33e3499a654c91d2e02abee72d16f
Ranjita Thapa Chhetri <ranjita.231526@ncit.edu.np>
2025-12-15 21:12:23 +0545

    adding identity_operator.py file

---
+++ identity_operator.py
@@ -0,0 +1,12 @@
+x = ["apple" , "banana"]
+y = ["apple" , "banana"]
+print( x is y)
+print( x is not y)
+print( x == y)
+print( x != y)
+
+
+a = [1, 2, 3]
+b = [1, 2, 3]
+print(a == b)
+print(a is b)
