3ec5177fd50222fc953bf335a94072330230e36c
Ranjita Thapa Chhetri <ranjita.231526@ncit.edu.np>
2025-12-15 21:30:06 +0545

    adding change_list_item.py file

---
+++ change_list_item.py
@@ -0,0 +1,10 @@
+thislist = ["apple", "banana", "cherry" , "orange", "kiwi", "melon", "mango"]
+# thislist[1] = "blackcurrant"
+# print(thislist)
+
+# thislist[2:5] = ["watermelon", "papaya"]
+# print(thislist)
+
+thislist = ["apple", "banana", "cherry"]
+thislist[1:2] = ["blackcurrant", "watermelon"]
+print(thislist)
