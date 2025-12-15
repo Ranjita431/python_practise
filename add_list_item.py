688122fe798229ffe9f005da103655c29da22cfc
Ranjita Thapa Chhetri <ranjita.231526@ncit.edu.np>
2025-12-15 21:39:31 +0545

    adding add_list_item.py file

---
+++ add_list_item.py
@@ -0,0 +1,7 @@
+thislist = ["apple", "banana", "cherry"]
+# thislist.append("orange")
+
+# thislist.insert(1, "orange")
+topical =["mango" , "pineapple" , "grapes"]
+thislist.extend(topical)
+print(thislist)
