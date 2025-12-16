fe58ea6aa2f6f73d03b119227a8b33ce7f7545fd
Ranjita Thapa Chhetri <ranjita.231526@ncit.edu.np>
2025-12-16 23:23:14 +0545

    adding sort.py file

---
+++ sort.py
@@ -0,0 +1,15 @@
+# thislist = ["orange", "banana", "cherry", "kiwi", "melon", "mango"]
+# thislist.sort()
+# print(thislist)
+
+# thislist = [100, 50, 65, 82, 23]
+# thislist.sort()
+# print(thislist)
+
+# thislist = ["orange", "banana", "cherry", "kiwi", "melon", "mango"]
+# thislist.sort(reverse=True)
+# print(thislist)
+
+thislist = [100, 50, 65, 82, 23]
+thislist.sort(reverse=True)
+print(thislist)
