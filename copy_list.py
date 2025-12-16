7fa7a98a0c1912d405387caec9376ca405acddf1
Ranjita Thapa Chhetri <ranjita.231526@ncit.edu.np>
2025-12-16 23:27:18 +0545

    adding copy_list.py

---
+++ copy_list.py
@@ -0,0 +1,11 @@
+# thislist = ["apple", "banana", "cherry"]
+# mylist = thislist.copy()
+# print(mylist)
+
+# thislist = ["apple", "banana", "cherry"]
+# newlist = list(thislist)
+# print(newlist)
+
+thislist = ["apple", "banana", "cherry"]
+mylist = thislist[:]
+print(mylist)
