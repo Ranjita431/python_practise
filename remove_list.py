15511a5ef8e36a69770e5293a1ee28cc5a787caa
Ranjita Thapa Chhetri <ranjita.231526@ncit.edu.np>
2025-12-16 23:06:06 +0545

    adding remove_list.py file

---
+++ remove_list.py
@@ -0,0 +1,11 @@
+thislist = ["apple", "banana", "cherry"]
+# thislist = ["apple", "banana", "cherry" ,"banana" , "orange"]
+# thislist.remove("banana")
+
+# thislist.pop()
+# thislist.pop(1)
+
+# del thislist[0]
+del thislist
+thislist.clear()
+print(thislist)
