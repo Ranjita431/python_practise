348f6122c87d6503078aa5599dddeac605f12374
Ranjita Thapa Chhetri <ranjita.231526@ncit.edu.np>
2025-12-17 22:26:17 +0545

    addint sets.py file

---
+++ sets.py
@@ -0,0 +1,9 @@
+# thisset = {"apple", "banana", "cherry"}
+# print(thisset)
+
+
+# thisset = {"apple", "banana", "cherry" , True, 1, 3.14}
+# print(thisset)
+
+thisset = {"apple", "banana", "cherry", False, True , 0}
+print(thisset)
