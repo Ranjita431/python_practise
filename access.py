577324869751ae1f42bbb706f2e49e61808c49d0
Ranjita Thapa Chhetri <ranjita.231526@ncit.edu.np>
2025-12-17 22:02:21 +0545

    adding access.py file

---
+++ access.py
@@ -0,0 +1,20 @@
+# thistuple = ["apple", "banana", "cherry"]
+# mytuple = thistuple.copy()
+# print(mytuple)
+
+# thistuple = ["apple", "banana", "cherry"]
+# print(thistuple[-1])
+
+thistuple = ("apple", "banana" , "cherry", "orange", "kiwi", "melon", "mango")
+# print(thistuple[2:5])
+
+# print(thistuple[-4:-1])
+# print(thistuple[:4])
+# print(thistuple[2:])
+# print(thistuple[-4:])
+# print(thistuple[:])
+
+thistuple = ("apple", "banana", "cherry")
+if "apple" in thistuple:
+    print("Yes, 'apple' is in the tuple")
+
