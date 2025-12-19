8a0a73a22e0809d868422eae334ddbdb3eb332f2
Ranjita Thapa Chhetri <ranjita.231526@ncit.edu.np>
2025-12-19 21:18:45 +0545

    adding r.py file

---
+++ r.py
@@ -0,0 +1,20 @@
+# thisdict = {
+#     "brand": "Ford",
+#     "electric": False,
+#     "year": 1964,
+# }
+# thisdict.pop("electric")
+# print(thisdict)
+
+thisdict = {
+    "brand": "Ford",
+    "electric": False,
+    "year": 1964,
+}
+# thisdict.popitem()
+# print(thisdict)
+# del thisdict["electric"]
+# print(thisdict)
+
+thisdict.clear()
+print(thisdict)
