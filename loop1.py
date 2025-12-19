2c9233997f8b0015318ec76b0bb8c65dd95cd008
Ranjita Thapa Chhetri <ranjita.231526@ncit.edu.np>
2025-12-19 21:22:31 +0545

    adding loop1.py file

---
+++ loop1.py
@@ -0,0 +1,11 @@
+thisdist = {
+    "name": "loop1",
+    "version": "1.0.0",
+    "description": "A simple loop example",
+    "author": "Your Name",
+}  
+# for x in thisdist:
+#     print(x)
+
+for x in this.dist.values():
+    print(x)
