110fce59c5e8bdd3e4ad5c50b4418198d4c2b53c
Ranjita Thapa Chhetri <ranjita.231526@ncit.edu.np>
2025-12-20 23:01:10 +0545

    adding arguments.py file

---
+++ arguments.py
@@ -0,0 +1,34 @@
+# def my_function(fname):
+#     print(fname + "refsnes")
+
+#     my_function("Emil ")
+#     my_function("Tobias ")
+#     my_function("Linus ")
+
+# def my_function(name):
+#     print("hello" , name)
+#     my_function("Alice")
+
+# def my_function(fname , lname):
+#     print(fname + " " + lname)
+# my_function("Emil" , "Refsnes")
+
+#  print(fname + " " + lname)
+# my_function("Emil" , "Refsnes")
+
+# def my_functtion(name = "friend"):
+#     print("hello " + name)
+# my_functtion("Alice")
+# my_functtion()  # uses default value
+# my_functtion("Bob")
+
+# def my_function(country = "Norway"):
+#     print("I am from " + country)
+# my_function("Sweden")
+# my_function("India")
+# my_function()
+# my_function("Brazil")
+
+def my_function(animal , name):
+    print("I have a " + animal + " named " + name)
+my_function("dog" , "Buddy")
