30c4d3d0f692610dc8bfa45210cb78cb966dd694
Ranjita Thapa Chhetri <ranjita.231526@ncit.edu.np>
2025-12-20 22:55:38 +0545

    adding function.py file

---
+++ function.py
@@ -0,0 +1,27 @@
+# def my_function():
+#     print("Hello, World!")
+#     my_function()
+#     my_function()
+#     my_function()
+
+# temp1 = 77
+# celsius = (temp1 - 32) * 5.0 / 9.0
+# print(celsius)
+
+# temp2 = 95
+# celsius = (temp2 - 32) * 5.0 / 9.0
+# print(celsius)
+
+# temp3 = 50
+# celsius = (temp3 - 32) * 5.0 / 9.
+# print(celsius)
+
+# def fahrenheit_to_celsius(temp_f):
+#     celsius = (temp_f - 32) * 5.0 / 9.0
+#     return celsius
+
+def get_greeting():
+    return "hello from a function"
+
+message = get_greeting()
+print(message)
