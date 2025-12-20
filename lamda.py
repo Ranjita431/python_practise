4ae7a06aa7ff8416e6fe56306759dbd8c378bf75
Ranjita Thapa Chhetri <ranjita.231526@ncit.edu.np>
2025-12-20 23:18:43 +0545

    adding lamda.py file

---
+++ lamda.py
@@ -0,0 +1,49 @@
+x = lambda a : a + 10
+print(x(5))  # Output: 15
+
+x = lambda a, b : a * b
+print(x(5, 6))  # Output: 30
+
+x = lambda a, b, c : a + b + c
+print(x(5, 6, 2))  # Output: 13
+
+def myfunc(n):
+    return lambda a : a * n
+
+def myfunc(n):
+    return lambda a : a * n
+doubler = myfunc(2)
+tripler = myfunc(3)
+
+
+def myfunc(n):
+  return lambda a : a * n
+
+mydoubler = myfunc(2)
+
+print(mydoubler(11))
+
+
+def myfunc(n):
+  return lambda a : a * n
+
+mytripler = myfunc(3)
+
+print(mytripler(11))
+
+
+
+def myfunc(n):
+  return lambda a : a * n
+
+mydoubler = myfunc(2)
+mytripler = myfunc(3)
+
+print(mydoubler(11))
+print(mytripler(11))
+
+
+
+numbers = [1, 2, 3, 4, 5]
+doubled = list(map(lambda x: x * 2, numbers))
+print(doubled)
