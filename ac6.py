0119d2d9dc9f5e659c03d854b237268260f864e2
Ranjita Thapa Chhetri <ranjita.231526@ncit.edu.np>
2026-01-03 22:16:55 +0545

    adding ac6.py file

---
+++ ac6.py
@@ -0,0 +1,9 @@
+
+thisdict = {
+    "brand": "Ford",
+    "electric": False,
+    "year": 1964,
+    "colors": ["red", "white", "blue"]
+}
+if "model" in thisdict:
+    print("Yes, 'model' is one of the keys in the thisdict dictionary")
