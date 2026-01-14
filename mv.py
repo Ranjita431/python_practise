71a1070e95e6ab6e05b1ed090f66321a88e70a33
Ranjita Thapa Chhetri <ranjita.231526@ncit.edu.np>
2026-01-14 23:21:49 +0545

    adding mv.py file

---
+++ mv.py
@@ -0,0 +1,16 @@
+x , y , z = 5, "John", 3.5
+print(x)
+print(y)
+print(z)
+
+x = y = z = "Orange"
+print(x)
+print(y)
+print(z)
+print(id(x))
+print(id(y))
+print(id(z))
+
+fruits = ["apple", "banana", "cherry"]
+x, y, z = fruits
+print(x)
