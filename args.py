8e53f151a4ce1abb98a62fb92e672d3d27584a2d
Ranjita Thapa Chhetri <ranjita.231526@ncit.edu.np>
2026-03-07 22:22:42 +0545

    changing args.py

--- args.py
+++ args.py
@@ -26,4 +26,6 @@ def my_function(*numbers):
     return total
 print(my_function(1, 2, 3))          # Output: 6
 print(my_function(10, 20, 30, 40))   # Output: 100
-print(my_function(5, 15))            # Output: 20
+print(my_function(5, 15))    
+# Output: 20
+print(my_function(6,17))
