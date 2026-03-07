15419cdb765a029823785eb2ae24232dd6db6c31
Ranjita Thapa Chhetri <ranjita.231526@ncit.edu.np>
2026-03-07 22:23:18 +0545

    Uncomment my_function to print greeting and args

--- args.py
+++ args.py
@@ -12,11 +12,11 @@
 
 #     my_function("apple", "banana", "cherry")
 
-# def my_function(greeting , *args):
-#     print(greeting)
-#     for arg in args:
-#         print(arg)
-# my_function("Hello!" , "apple" , "banana" , "cherry")
+ def my_function(greeting , *args):
+     print(greeting)
+     for arg in args:
+         print(arg)
+ my_function("Hello!" , "apple" , "banana" , "cherry")
 
 
 def my_function(*numbers):
