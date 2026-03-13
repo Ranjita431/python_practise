849f1323440d62cf9be0f211b7d78f1bb06b53b0
Ranjita Thapa Chhetri <ranjita.231526@ncit.edu.np>
2026-03-13 23:43:58 +0545

    Comment out dictionary code in Dictionaries.py
    Comment out the dictionary declaration and print statements.

--- Dictionaries.py
+++ Dictionaries.py
@@ -6,13 +6,13 @@
  print(thisdict)
  print(thisdict["brand"])
 
- thisdict= {
-     "brand": "Ford",
-     "model": "Mustang",
-     "year": 1964,
-     "year": 2020
- }
- print(thisdict)
+ #thisdict= {
+ #    "brand": "Ford",
+#     "model": "Mustang",
+   #  "year": 1964,
+  #   "year": 2020
+ #}
+# print(thisdict)
 
 #thisdict= {
    # "brand": "Ford",
