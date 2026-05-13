da380eee6c744fd68d15c679bf6dab27569ba042
Ranjita Thapa Chhetri <ranjita.231526@ncit.edu.np>
2026-05-13 21:38:22 +0545

    Comment out dictionary and update year value
    Comment out the dictionary initialization and update the year key.

--- Dictionaries.py
+++ Dictionaries.py
@@ -6,9 +6,9 @@
  #print(thisdict)
  #print(thisdict["brand"])
 
- thisdict= {
-     "brand": "Ford",
-   "model": "Mustang",
+# thisdict= {
+  #   "brand": "Ford",
+  # "model": "Mustang",
      "year": 1964,
      "year": 2020
 }
