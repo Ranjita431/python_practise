5547ebd0ecf5f6caa94a5b97ebfdc66ed9be58b7
Ranjita Thapa Chhetri <ranjita.231526@ncit.edu.np>
2026-05-14 21:45:01 +0545

    Comment out original my_function for debugging
    Comment out the original my_function definition and its print statements for debugging purposes.

--- args.py
+++ args.py
@@ -3,12 +3,12 @@
  #my_function("Emil", "Tobias", "Linus")
 
 
- def my_function(*args): not working
-     print("Type",type(args))
-     print("First argument:", args[0])
-     print("Second argument:", args[1])
-     print("Third argument:", args[2])
-     print("All arguments:", args)
+# def my_function(*args): not working
+ #    print("Type",type(args))
+  #   print("First argument:", args[0])
+   #  print("Second argument:", args[1])
+    # print("Third argument:", args[2])
+     #print("All arguments:", args)
 
   my_function("apple", "banana", "cherry")
 
