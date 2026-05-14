c725d692f48832831717c13bc3278b5e67c29d8c
Ranjita Thapa Chhetri <ranjita.231526@ncit.edu.np>
2026-05-14 21:44:21 +0545

    Add function calls to demonstrate my_function usage

--- arg4.py
+++ arg4.py
@@ -3,6 +3,7 @@ def my_function(*numbers):
     for num in numbers:
         total += num
     return total
+    
 print(my_function(1, 2, 3))          # Output: 6
 print(my_function(10, 20, 30, 40))   # Output: 100
 print(my_function(5, 15))            # Output: 20
