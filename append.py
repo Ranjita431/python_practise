b8ae1a9fb6e343c93bea22931135e3b0841be2b6
Ranjita Thapa Chhetri <ranjita.231526@ncit.edu.np>
2026-05-15 21:29:54 +0545

    Comment out file reading after appending content
    Comment out the file reading section in append.py.

--- append.py
+++ append.py
@@ -2,5 +2,5 @@ with open("demofile.txt", "a") as f:
   f.write("Now the file has more content!")
 
 #open and read the file after the appending:
-with open("demofile.txt") as f:
-  print(f.read())
+#with open("demofile.txt") as f:
+ # print(f.read())
