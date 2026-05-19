9cfe0e81c8001cd56b07c5660c3844a6520c1e45
Ranjita Thapa Chhetri <ranjita.231526@ncit.edu.np>
2026-05-19 22:10:40 +0545

    Uncomment tuple slicing print statement

--- access.py
+++ access.py
@@ -10,7 +10,7 @@ thistuple = ("apple", "banana" , "cherry", "orange", "kiwi", "melon", "mango")
 
  print(thistuple[-4:-1])
  print(thistuple[:4])
-# print(thistuple[2:])
+ print(thistuple[2:])
 # print(thistuple[-4:])
 # print(thistuple[:])
 
