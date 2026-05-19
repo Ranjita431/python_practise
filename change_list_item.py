b94e97ef4b8293f9d0dbc81fc720dd779eb9454e
Ranjita Thapa Chhetri <ranjita.231526@ncit.edu.np>
2026-05-19 22:16:38 +0545

    Uncomment list modification and print statements

--- change_list_item.py
+++ change_list_item.py
@@ -1,6 +1,6 @@
-# thislist = ["apple", "banana", "cherry" , "orange", "kiwi", "melon", "mango"]
-# thislist[1] = "blackcurrant"
-# print(thislist)
+ thislist = ["apple", "banana", "cherry" , "orange", "kiwi", "melon", "mango"]
+ thislist[1] = "blackcurrant"
+ print(thislist)
 
 # thislist[2:5] = ["watermelon", "papaya"]
 # print(thislist)
