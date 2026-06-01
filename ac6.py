1965a69f709975609467e78875f66b0356295393
Ranjita Thapa Chhetri <ranjita.231526@ncit.edu.np>
2026-06-01 21:23:28 +0545

    Update ac6.py

--- ac6.py
+++ ac6.py
@@ -5,5 +5,5 @@
   #  "year": 1964,
    # "colors": ["red", "white", "blue"]
 #}
-if "model" in thisdict:
+#if "model" in thisdict:
     print("Yes, 'model' is one of the keys in the thisdict dictionary")
