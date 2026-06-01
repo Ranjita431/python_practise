a1c8b5c7ad26b860534378751c8a60d902d072ad
Ranjita Thapa Chhetri <ranjita.231526@ncit.edu.np>
2026-06-01 21:24:13 +0545

    Update ac6.py

--- ac6.py
+++ ac6.py
@@ -3,7 +3,7 @@ thisdict = {
     "brand": "Ford",
     "electric": False,
     "year": 1964,
-   # "colors": ["red", "white", "blue"]
-#}
-#if "model" in thisdict:
-   # print("Yes, 'model' is one of the keys in the thisdict dictionary")
+    "colors": ["red", "white", "blue"]
+}
+if "model" in thisdict:
+    print("Yes, 'model' is one of the keys in the thisdict dictionary")
