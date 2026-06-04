ad55b04b31911c7df4f7fe4a186b1cb2bce9848f
Ranjita Thapa Chhetri <ranjita.231526@ncit.edu.np>
2026-06-04 21:59:20 +0545

    Comment out duplicate thistuple definition
    Comment out the redefinition of thistuple.

--- access.py
+++ access.py
@@ -2,7 +2,7 @@
  mytuple = thistuple.copy()
  print(mytuple)
 
- thistuple = ["apple", "banana", "cherry"]
+ #thistuple = ["apple", "banana", "cherry"]
  print(thistuple[-1])
 
 thistuple = ("apple", "banana" , "cherry", "orange", "kiwi", "melon", "mango")
