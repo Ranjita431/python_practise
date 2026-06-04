baee8f8e684c75b2f487c356e505baaa2aaabfad
Ranjita Thapa Chhetri <ranjita.231526@ncit.edu.np>
2026-06-04 21:59:30 +0545

    Comment out print statement for thistuple
    Comment out the print statement for the last element of thistuple.

--- access.py
+++ access.py
@@ -3,7 +3,7 @@
  print(mytuple)
 
  #thistuple = ["apple", "banana", "cherry"]
- print(thistuple[-1])
+ #print(thistuple[-1])
 
 thistuple = ("apple", "banana" , "cherry", "orange", "kiwi", "melon", "mango")
  print(thistuple[2:5])
