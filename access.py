0ae4033ea3a81cd4eb3d3d771c62226b3c50640d
Ranjita Thapa Chhetri <ranjita.231526@ncit.edu.np>
2026-06-04 21:59:41 +0545

    Update access.py

--- access.py
+++ access.py
@@ -5,7 +5,7 @@
  #thistuple = ["apple", "banana", "cherry"]
  #print(thistuple[-1])
 
-thistuple = ("apple", "banana" , "cherry", "orange", "kiwi", "melon", "mango")
+#thistuple = ("apple", "banana" , "cherry", "orange", "kiwi", "melon", "mango")
  print(thistuple[2:5])
 
  print(thistuple[-4:-1])
