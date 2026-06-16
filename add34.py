75e6ec51b06c381ba7279224f210899593a2bd49
Ranjita Thapa Chhetri <ranjita.231526@ncit.edu.np>
2026-06-16 22:10:20 +0545

    add

---
+++ add34.py
@@ -0,0 +1,9 @@
+name = "Ranjita"    # String
+age = 20            # Integer
+height = 5.3        # Float
+is_student = True   # Boolean
+
+print(type(name))
+print(type(age))
+print(type(height))
+print(type(is_student))
