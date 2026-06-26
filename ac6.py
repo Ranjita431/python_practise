6b7dd969ac4bbda6f9cfcfc2f2e12dd775a87b8d
Ranjita Thapa Chhetri <ranjita.231526@ncit.edu.np>
2026-06-26 23:23:46 +0545

    Update ac6.py

--- ac6.py
+++ ac6.py
@@ -4,6 +4,6 @@ thisdict = {
  #   "electric": False,
   #  "year": 1964,
   #  "colors": ["red", "white", "blue"]
-}
+#}
 if "model" in thisdict:
     print("Yes, 'model' is one of the keys in the thisdict dictionary")
