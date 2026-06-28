5ec04a42919721c95ea67f1c1f2dfd08de637f35
Ranjita Thapa Chhetri <ranjita.231526@ncit.edu.np>
2026-06-28 22:47:18 +0545

    Update arguments.py

--- arguments.py
+++ arguments.py
@@ -17,7 +17,7 @@
 # my_function("Emil" , "Refsnes")
 
 # def my_functtion(name = "friend"):
-#     print("hello " + name)
+    print("hello " + name)
  my_functtion("Alice")
  my_functtion()  # uses default value
  my_functtion("Bob")
