eed43877a845e14640677fa521ac07ca254637c1
Ranjita Thapa Chhetri <ranjita.231526@ncit.edu.np>
2026-06-28 22:46:57 +0545

    Update arguments.py

--- arguments.py
+++ arguments.py
@@ -19,7 +19,7 @@
 # def my_functtion(name = "friend"):
 #     print("hello " + name)
 # my_functtion("Alice")
-# my_functtion()  # uses default value
+ my_functtion()  # uses default value
  my_functtion("Bob")
 
  def my_function(country = "Norway"):
