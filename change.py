5e8a4d271007816d2b9d9dd14d109a6ea9776438
Ranjita Thapa Chhetri <ranjita.231526@ncit.edu.np>
2026-07-21 23:09:35 +0545

    Uncomment code to update year in dictionary
    Uncomment dictionary initialization and year update.

--- change.py
+++ change.py
@@ -2,9 +2,9 @@
 #     "brand": "Ford",
 #     "model": "Mustang",
 #     "year": 1964
-# }
-# thisdict["year"] = 2020
-# print(thisdict)
+ }
+ thisdict["year"] = 2020
+ print(thisdict)
 
 thisdict = {
     "brand": "Ford",
